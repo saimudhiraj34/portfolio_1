c6f46435259f9d065fe12cfa1e4d429cebb06903
saimudhiraj34 <saimudhiraj6410@gmail.com>
2025-10-11 22:49:04 +0530

    responsive

--- Portfolio/src/Firstcontainer.jsx
+++ Portfolio/src/Firstcontainer.jsx
@@ -3,8 +3,6 @@ import "./Firstcontainer.css";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import { useRef } from "react";
 
-
-
 export const Firstcontainer = () => {
   const skills = [
     "Java",
@@ -14,13 +12,13 @@ export const Firstcontainer = () => {
     "MongoDB",
     "Express",
     "Sql",
-    "problem_solving"
+    "problem_solving",
   ];
   const [currentSkillIndex, setCurrentSkillIndex] = useState(0);
   const [displayText, setDisplayText] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
   const [isUserInteracting, setIsUserInteracting] = useState(false);
-   const intervalRef = useRef(null);
+  const intervalRef = useRef(null);
 
   useEffect(() => {
     const currentSkill = skills[currentSkillIndex];
@@ -46,8 +44,6 @@ export const Firstcontainer = () => {
     return () => clearTimeout(timer);
   }, [displayText, isDeleting, skills, currentSkillIndex]);
 
-
-
   const slides = [
     {
       id: 1,
@@ -55,11 +51,21 @@ export const Firstcontainer = () => {
         <div className="slide-1">
           <div className="navbar">
             <ul>
-              <a href="#home"><li>Home</li></a>
-              <a href="#skills"><li>Skills</li></a>
-              <a href="#projects"><li>Project</li></a>
-              <a href="#certificate"><li>Certificates</li></a>
-              <a href="#contact"><li>Contact</li></a>
+              <a href="#home">
+                <li>Home</li>
+              </a>
+              <a href="#skills">
+                <li>Skills</li>
+              </a>
+              <a href="#projects">
+                <li>Project</li>
+              </a>
+              <a href="#certificate">
+                <li>Certificates</li>
+              </a>
+              <a href="#contact">
+                <li>Contact</li>
+              </a>
             </ul>
           </div>
           <div className="slide-container">
@@ -123,12 +129,13 @@ export const Firstcontainer = () => {
                     <FaGithub />
                   </a>
                   <a
-                    href="https://linkedin.com/in/yourlinkedin"
+                    href="https://www.linkedin.com/in/marikanti-sai-kumar-438866287"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
                     <FaLinkedin />
                   </a>
+
                   <a href="mailto:youremail@example.com">
                     <FaEnvelope />
                   </a>
@@ -136,7 +143,7 @@ export const Firstcontainer = () => {
               </div>
             </div>
             <div className="slide-1_pic">
-            <img  className="sai_pic"src="/sai_pic.png" alt="Sai Kumar" />
+              <img className="sai_pic" src="/sai_pic.png" alt="Sai Kumar" />
             </div>
           </div>
         </div>
@@ -148,82 +155,112 @@ export const Firstcontainer = () => {
         <div className="slide-2">
           <div className="about-container">
             <div className="slide-2_pic">
-               <img  className="sai_pic2"src="/sai_pic.png" alt="Sai Kumar" />
+              <img className="sai_pic2" src="/sai_pic.png" alt="Sai Kumar" />
             </div>
             <div className="about-details">
               <h2>Introducing Me</h2>
               <p>
-                My name is <s>Sai Kumar</s>, and I am an <s>AI & Machine Learning</s> student
-                with a passion for learning new technologies and solving
-                <s>real-world</s> problems. I completed my diploma at <s>TRR College</s> from
-               <s> 2020 </s>to <s>2023</s> , which laid a strong foundation in <s>computer science
-                and programming</s>. Currently, I am pursuing my <s> B.Tech </s>at <s>Anurag
-                University </s> from <s>2023</s> to <s>2026</s>, where I have maintained a strong
-                academic record with an <s>8.35 GPA</s>. Alongside my studies, I have
-                actively honed my <s>problem-solving</s> skills by completing over <s>300+</s>
-                problems on <s>LeetCode </s>and <s>150+</s> problems on <s>GeeksforGeeks</s>. I have
-                undertaken several <s>projects</s>based on my <s>own problem statements</s>,
-                which allowed me to practically apply concepts and enhance my
-                learning. My experience with <s>MERN stack</s> projects has
-                strengthened my expertise in <s>authentication</s>,<s>API development</s>,
-                <s>schema design</s>, and working with both <s>SQL</s> and <s>NoSQL databases</s>. I
-                am also proficient in <s>Java </s>and regularly solve problems to
-                improve my coding and logical thinking abilities. Participating
-                in hackathons has further enhanced my hands-on experience,
-                <s>teamwork</s>, and ability to tackle challenges creatively. I am
-                continuously exploring new tools and <s>technologies</s> to grow as a
-                developer and create meaningful solutions. My journey reflects a
-                blend of <s>academics</s>, <s>practical experience </s>, and a relentless drive
-                to learn and innovate in the field of <s>AI & ML</s>.
+                My name is <s>Sai Kumar</s>, and I am an{" "}
+                <s>AI & Machine Learning</s> student with a passion for learning
+                new technologies and solving
+                <s>real-world</s> problems. I completed my diploma at{" "}
+                <s>TRR College</s> from
+                <s> 2020 </s>to <s>2023</s> , which laid a strong foundation in{" "}
+                <s>computer science and programming</s>. Currently, I am
+                pursuing my <s> B.Tech </s>at <s>Anurag University </s> from{" "}
+                <s>2023</s> to <s>2026</s>, where I have maintained a strong
+                academic record with an <s>8.35 GPA</s>. Alongside my studies, I
+                have actively honed my <s>problem-solving</s> skills by
+                completing over <s>300+</s>
+                problems on <s>LeetCode </s>and <s>150+</s> problems on{" "}
+                <s>GeeksforGeeks</s>. I have undertaken several <s>projects</s>
+                based on my <s>own problem statements</s>, which allowed me to
+                practically apply concepts and enhance my learning. My
+                experience with <s>MERN stack</s> projects has strengthened my
+                expertise in <s>authentication</s>,<s>API development</s>,
+                <s>schema design</s>, and working with both <s>SQL</s> and{" "}
+                <s>NoSQL databases</s>. I am also proficient in <s>Java </s>and
+                regularly solve problems to improve my coding and logical
+                thinking abilities. Participating in hackathons has further
+                enhanced my hands-on experience,
+                <s>teamwork</s>, and ability to tackle challenges creatively. I
+                am continuously exploring new tools and <s>technologies</s> to
+                grow as a developer and create meaningful solutions. My journey
+                reflects a blend of <s>academics</s>,{" "}
+                <s>practical experience </s>, and a relentless drive to learn
+                and innovate in the field of <s>AI & ML</s>.
               </p>
             </div>
           </div>
         </div>
       ),
     },
-  {
-  id: 3,
-  content: (
-    <div className="slide-3">
-      <div className="education-tree">
-        <div className="tree-line"></div>
+    {
+      id: 3,
+      content: (
+        <div className="slide-3">
+          <div className="education-tree">
+            <div className="tree-line"></div>
 
-        <div className="tree-branch left-branch">          
-          <div className="tree-box">
-            <h3>College</h3>
-           <p><strong>College:</strong> Anurag University</p>
-            <p><strong>Course:</strong> B.Tech in AI & ML</p>
-            <p><strong>Years:</strong> 2023 - 2026</p>
-            <p><strong>Place:</strong>Ghatkesar,Hyderabad</p>
-          </div>
-          <div className="branch-line"></div>
-        </div>
+            <div className="tree-branch left-branch">
+              <div className="tree-box">
+                <h3>College</h3>
+                <p>
+                  <strong>College:</strong> Anurag University
+                </p>
+                <p>
+                  <strong>Course:</strong> B.Tech in AI & ML
+                </p>
+                <p>
+                  <strong>Years:</strong> 2023 - 2026
+                </p>
+                <p>
+                  <strong>Place:</strong>Ghatkesar,Hyderabad
+                </p>
+              </div>
+              <div className="branch-line"></div>
+            </div>
 
-        <div className="tree-branch right-branch">
-          <div className="branch-line"></div>
-          <div className="tree-box">
-            <h3>College</h3>
-            <p><strong>College:</strong>T.R.R polytechnic college</p>
-            <p><strong>Course:</strong>Computer Science and Engineering</p>
-            <p><strong>Years:</strong> 2020 - 2023</p>
-             <p><strong>Place:</strong>Meerpet,Hyderabad</p>
+            <div className="tree-branch right-branch">
+              <div className="branch-line"></div>
+              <div className="tree-box">
+                <h3>College</h3>
+                <p>
+                  <strong>College:</strong>T.R.R polytechnic college
+                </p>
+                <p>
+                  <strong>Course:</strong>Computer Science and Engineering
+                </p>
+                <p>
+                  <strong>Years:</strong> 2020 - 2023
+                </p>
+                <p>
+                  <strong>Place:</strong>Meerpet,Hyderabad
+                </p>
+              </div>
+            </div>
+            <div className="tree-branch left-branch-down">
+              <div className="tree-box">
+                <h3>Schooling</h3>
+                <p>
+                  <strong>School:</strong>Sri Sai Grammer High School
+                </p>
+                <p>
+                  <strong>Years:</strong> 2018 - 2020
+                </p>
+                <p>
+                  <strong>Board:</strong>ssc
+                </p>
+                <p>
+                  <strong>Place:</strong>Hyderabad
+                </p>
+              </div>
+              <div className="branch-line"></div>
+            </div>
           </div>
         </div>
-        <div className="tree-branch left-branch-down">          
-          <div className="tree-box">
-            <h3>Schooling</h3>
-            <p><strong>School:</strong>Sri Sai Grammer High School</p>
-            <p><strong>Years:</strong> 2018 - 2020</p>
-            <p><strong>Board:</strong>ssc</p>
-             <p><strong>Place:</strong>Hyderabad</p>
-          </div>
-          <div className="branch-line"></div>
-        </div>
-      </div>
-    </div>
-  ),
-}
-
+      ),
+    },
   ];
 
   //   useEffect(() => {
@@ -234,7 +271,6 @@ export const Firstcontainer = () => {
   //   return () => clearInterval(intervalRef.current);
   // }, [isUserInteracting, slides.length]);
 
-
   // const handleUserClick = (index) => {
   //   setIsUserInteracting(true);
   //   setCurrentIndex(index);
@@ -242,34 +278,34 @@ export const Firstcontainer = () => {
   //   setTimeout(() => setIsUserInteracting(false),10000);
   // };
   const [currentIndex, setCurrentIndex] = useState(0);
-  const goToSlide = (index) => {setCurrentIndex(index)};
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
 
   return (
     <>
+      <div className="container" id="home">
+        <div
+          className="slides-wrapper"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {slides.map((slide) => (
+            <div key={slide.id} className="slide">
+              {slide.content}
+            </div>
+          ))}
+        </div>
 
-    <div className="container" id="home">
-      <div
-        className="slides-wrapper"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-      >
-        {slides.map((slide) => (
-          <div key={slide.id} className="slide" >
-            {slide.content}
-          </div>
-        ))}
+        <div className="dots">
+          {slides.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${currentIndex === index ? "active" : ""}`}
+              onClick={() => goToSlide(index)}
+            ></span>
+          ))}
+        </div>
       </div>
-
-      <div className="dots">
-        {slides.map((_, index) => (
-          <span
-            key={index}
-            className={`dot ${currentIndex === index ? "active" : ""}`}
-            onClick={() => goToSlide(index)}
-          ></span>
-        ))}
-      </div>
-
-    </div>
     </>
   );
 };
